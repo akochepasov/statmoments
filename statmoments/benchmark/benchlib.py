cc27b0c82e012fea6e54c9d3f0e6ce002e4ab508
Petr Gladkikh <petrglad@gmail.com>
2023-12-04 01:13:20 +0100

    Performance benchmarks cleanup
    Note that the shown stats (timing columns) have changed.

--- statmoments/benchmark/benchlib.py
+++ statmoments/benchmark/benchlib.py
@@ -1,4 +1,4 @@
-
+import gc
 from itertools import product
 from os import urandom as _urandom
 from binascii import b2a_hex
@@ -26,7 +26,7 @@ def make_hypotheses(hlen):
   return bytes2classifiers(_urandom((hlen + 7) // 8))[:hlen]
 
 
-class engine_factory(object):
+class EngineFactory(object):
   def __init__(self, make_trace, make_classifiers, factory, **options):
     self._gen_trace = make_trace
     self._gen_classifiers = make_classifiers
@@ -37,7 +37,7 @@ class engine_factory(object):
     blk_sz = 8
     traces = np.empty((tr_count // blk_sz * blk_sz, tr_len))
     traces[:len(traces) // blk_sz, :] = self._gen_trace((len(traces) // blk_sz, tr_len))
-    traces.reshape(blk_sz, -1, tr_len)[:] = traces[:len(traces)//blk_sz]
+    traces.reshape(blk_sz, -1, tr_len)[:] = traces[:len(traces) // blk_sz]
     traces = np.random.permutation(traces)
     classifiers = [self._gen_classifiers(cl_len) for _ in range(len(traces))]
     return traces, classifiers
@@ -49,34 +49,35 @@ class engine_factory(object):
 def benchmark(benchset):
   number, repeat = 1, 1
 
-# Name        Implementation                     MB  tr_cnt   tr_len  cl_cnt   tr/sec   time res_time
-# bivar_m1    bivar_vtk(m(1, 1))                 50    5000      500       1      725    6.9      0.0
-# bivar_m1    bivar_vtk(m(1, 1))                124    5000     1000       1      132   37.7      0.0
-  print('{:12}{:30}{:>7}{:>8}{:>9}{:>8}{:>8}{:>7}{:>9}'.format(
-        'Kernel', 'Implementation', 'MB', 'tr_cnt', 'tr_len', 'cl_cnt', 'tr/sec', 'time', 'res_time'))
+  print('{:12}{:30}{:>7}{:>8}{:>9}{:>8}{:>8}{:>11}{:>10}{:>7}'.format(
+    'Kernel', 'Implementation', 'MB', 'tr_cnt', 'tr_len', 'cl_cnt', 'tr/sec', 'avg_update', 'avg_ttest', 'time'))
 
   for name, params in benchset:
-    for fctr, tr_count, tr_len, cl_count in product(*params):
-      tproc, ttest = [], []
+    for engine_factory, tr_count, tr_len, cl_count in product(*params):
+      count, proc_times, ttest_times = 0, [], []
+      assert repeat > 0
       for _ in range(repeat):
-        # Garbage collect memory after the prev iteration to free the existing memory
-        traces, engine = None, None
-        traces, classifiers = fctr.create_data(tr_count, tr_len, cl_count)
-        engine = fctr.create_engine(tr_len, cl_count)
+        traces, classifiers = engine_factory.create_data(tr_count, tr_len, cl_count)
+        engine = engine_factory.create_engine(tr_len, cl_count)
         for _ in range(number):
+          count += 1
           start = timer()
           engine.update(traces, classifiers)  # Streaming: layout and accumulator
-          tproc.append(timer() - start)
+          proc_times.append(timer() - start)
 
           start = timer()
-          for _ in ttests(engine):           # On-demand: t-tests
-            _ = None
-          ttest.append(timer() - start)
+          for _ in ttests(engine):  # On-demand: t-tests
+            pass
+          ttest_times.append(timer() - start)
 
-      mproc, mtt = min(tproc), min(ttest)
-      max_mom = ''.join(map(str, [engine.moment]*2))
+      total_time = sum(proc_times) + sum(ttest_times)
+      max_mom = str(engine.moment) * 2
       kname = '{}(m{})'.format(type(engine._impl).__name__, max_mom)
-      print("{:12}{:30}{:7d}{:8d}{:9d}{:8d}{:>8d}{:>7.1f}{:>9.1f}".format(
-          name, kname, engine.memory_size >> 20, tr_count, tr_len, cl_count,
-          int(tr_count / mproc), mproc, mtt))
+      print("{:12}{:30}{:7d}{:8d}{:9d}{:8d}{:>8.1f}{:>11.3f}{:>10.3f}{:>7.1f}".format(
+        name, kname, engine.memory_size >> 20, tr_count, tr_len, cl_count,
+                     count / total_time, min(proc_times) / count, min(ttest_times) / count, total_time))
+      # Force garbage collection
+      del traces
+      del engine
+      gc.collect()
   print()
