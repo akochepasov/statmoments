6b7d19fe33862ac3f5a935929e813aec84b6a615
anton <akss@me.com>
2024-02-22 12:12:57 -0800

    Converted command line call to pytest

--- statmoments/tests/__main__.py
+++ statmoments/tests/__main__.py
@@ -1,14 +1,9 @@
 
+import os
 import sys
-import unittest
-
-
-def run_alltests():
-  suite = unittest.TestLoader().discover('statmoments.tests', pattern='test_*.py')
-  return unittest.TextTestRunner(verbosity=1).run(suite)
+import pytest
 
 
 # Entrance point
 if __name__ == '__main__':
-  res = run_alltests()
-  sys.exit(res.wasSuccessful() is False)
+  sys.exit(pytest.main(["-v", os.path.dirname(__file__)]))
